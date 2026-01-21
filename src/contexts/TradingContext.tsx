2f0e55bbdb7e5090445a0b398ea473bdfd97ee5f
Lewis <hkg945@gmail.com>
2026-01-21 21:54:44 +0800

    fix: Add missing useState import

--- src/contexts/TradingContext.tsx
+++ src/contexts/TradingContext.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { createContext, useContext, useReducer, useCallback, useEffect, useRef } from 'react'
+import React, { createContext, useContext, useReducer, useCallback, useEffect, useRef, useState } from 'react'
 import {
   Symbol,
   Quote,
